1630ae1f76c7de6a1a41908c1b2b9072682157c2
Bruno Falcone <bnfalcone@yahoo.es>
2023-08-03 12:07:16 +0100

    Script to analyse convergence of GBSA

--- util/convergence.py
+++ util/convergence.py
@@ -28,41 +28,106 @@ df['gbsa_4_delta_total'] = df['gbsa_4_10_delta_total']
 
 df['gbsa_1_delta_total'] = df['gbsa_delta_total']
 
-print(df.columns)
 
-def get_std(j):
-	# Get standard deviation for tuple j in GBSA data
-	pass
+def get_std(d_names):
+	# Get standard deviation for dictionary of combinations vs iterations in GBSA data
 
-def get_avg(j):
-	# Get average for tuple j in GBSA data
+	for key, val in d_names.items():
 
-	k = [str(x) for x in j]
+		if len(val) == 1:
+			df['gbsa_%s_std' %key] = 0
+		else:			
+			df['gbsa_%s_std' %key] = df[val].std(axis=1)
 
-	name = '_'.join(k)
 
-	print(name)
+def get_avg(d):
+	# Get average for dictionary d in GBSA data
 
-	df['gbsa_%s' %name] = 0
+	for key, val in d.items():
 
-	for i in j:
-		df['gbsa_%s' %name] += df['gbsa_%s_delta_total' %i]		
+		df[key] = key
 
-	df['gbsa_%s' %name] /= len(j)	
+		for j in val:
 
-	return name
+			k = [str(x) for x in j]
 
-fig, ax = plt.subplots()
+			name = '_'.join(k)
 
-for i in range(1, len(a)+1):
+			print(name)
 
-	for j in list(combinations(a, i)):
-		name = get_avg(j)
+			df['gbsa_%s' %name] = 0
 
-		df[name] = i
+			for i in j:
+				df['gbsa_%s' %name] += df['gbsa_%s_delta_total' %i]		
 
-		ax.scatter(df[name], df['gbsa_%s' %name], s=1)
+			df['gbsa_%s' %name] /= len(j)	
+
+
+def make_dict_combinations(a):
+	# Make dict mapping elements in list a to list of combinations
+
+	d = dict()
+
+	for i in range(1, len(a)+1):
+
+		l = []
+
+		for j in list(combinations(a, i)):
+			l.append(j)
+
+		d[i] = l
+
+	return d
+
+
+def make_dict_names(d):
+	# Make dictionary mapping iterations to list of names
+
+	d_names = dict()
+
+	for key, val in d.items():
+
+		l = []
+
+		for j in val:
+
+			k = [str(x) for x in j]
+
+			name = 'gbsa_' + '_'.join(k)
+
+			l.append(name)
+
+		d_names[key] = l
+
+	return d_names
+
+
+def plot_std():
+	# Plot convergence graph of STD vs. No. simulations
+
+	fig, ax = plt.subplots()
+
+	for i in a:
+
+		ax.scatter(df[i], df['gbsa_%s_std' %i])
+
+	plt.xlabel('No. simulations')
+	plt.ylabel('STD in energy (kcal/mol)')
+
+	plt.xticks(a)
+
+	plt.savefig('convergence.pdf')
+
+d = make_dict_combinations(a)
+
+get_avg(d)
+
+d_names = make_dict_names(d)
+print(d_names)
+
+get_std(d_names)
+
+plot_std()
 
 print(df)
 
-plt.savefig('convergence.pdf')
