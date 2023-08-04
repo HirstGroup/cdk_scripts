c75b0fec99db5cd55332c2dee00a0ede1b0be260
Bruno Falcone <bnfalcone@yahoo.es>
2023-08-04 17:12:35 +0100

    Removed parsable from second call

--- run/run_md_ptraj_gbsa.sh
+++ run/run_md_ptraj_gbsa.sh
@@ -5,7 +5,7 @@ echo "complex = " $complex "repeat = " $repeat
 
 jobid=$(sbatch --parsable ~/scripts/1gpu.sh ~/cdk_scripts/standard_md.sh $complex "$repeat")
 
-sbatch --dependency=afterok:$jobid --parsable ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh $complex "$repeat"
+sbatch --dependency=afterok:$jobid ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh $complex "$repeat"
 
 
 
