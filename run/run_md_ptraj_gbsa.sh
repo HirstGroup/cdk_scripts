50100a58da676fdeda103ab48c5603aa5c6b2b81
Bruno Falcone <bnfalcone@yahoo.es>
2023-08-04 15:52:16 +0100

    Changed lig to complex

--- run/run_md_ptraj_gbsa.sh
+++ run/run_md_ptraj_gbsa.sh
@@ -1,7 +1,7 @@
 complex=$1
 repeat=${2:-""} # optional parameter to run repeats, e.g. _1 _2 etc
 
-echo "complex = " $ "repeat = " $repeat
+echo "complex = " $complex "repeat = " $repeat
 
 jobid=$(sbatch --parsable ~/scripts/1gpu.sh ~/cdk_scripts/standard_md.sh $complex "$repeat")
 
