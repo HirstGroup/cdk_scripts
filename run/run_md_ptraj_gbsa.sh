4b4360c955ebe8950e5b8eff572abef73ac5b5f1
bnfalcone4 <bnfalcone@yahoo.es>
2023-08-06 15:12:45 +0100

    Removed equi from part

--- run/run_md_ptraj_gbsa.sh
+++ run/run_md_ptraj_gbsa.sh
@@ -8,7 +8,7 @@ echo "complex = " $complex "repeat = " $repeat
 
 jobid=$(sbatch --parsable ~/scripts/1gpu.sh bash ~/cdk_scripts/standard_md{part}.sh $complex "$repeat")
 
-sbatch --dependency=afterok:$jobid ~/scripts/1cpu.sh bash ~/cdk_scripts/run/run_ptraj_gbsa.sh $complex "$repeat" "equi$part"
+sbatch --dependency=afterok:$jobid ~/scripts/1cpu.sh bash ~/cdk_scripts/run/run_ptraj_gbsa.sh $complex "$repeat" "$part"
 
 
 
