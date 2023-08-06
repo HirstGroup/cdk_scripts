28366cde1514c651724522f0dde6b7fc6a572510
bnfalcone4 <bnfalcone@yahoo.es>
2023-08-06 15:32:15 +0100

    Fixed part

--- run/run_md_ptraj_gbsa.sh
+++ run/run_md_ptraj_gbsa.sh
@@ -6,7 +6,7 @@ part=${3:-""} # optional parameter to run second part of equi with standard_md2
 
 echo "complex = " $complex "repeat = " $repeat
 
-jobid=$(sbatch --parsable ~/scripts/1gpu.sh bash ~/cdk_scripts/standard_md{part}.sh $complex "$repeat")
+jobid=$(sbatch --parsable ~/scripts/1gpu.sh bash ~/cdk_scripts/standard_md${part}.sh $complex "$repeat")
 
 sbatch --dependency=afterok:$jobid ~/scripts/1cpu.sh bash ~/cdk_scripts/run/run_ptraj_gbsa.sh $complex "$repeat" "$part"
 
