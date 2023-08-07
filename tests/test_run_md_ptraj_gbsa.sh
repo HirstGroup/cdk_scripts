3ad9aa83ba4f325bcd0a37b6d8d8a49545826fac
bnfalcone4 <bnfalcone@yahoo.es>
2023-08-07 13:28:01 +0100

    command line arguments

--- tests/test_run_md_ptraj_gbsa.sh
+++ tests/test_run_md_ptraj_gbsa.sh
@@ -5,6 +5,6 @@ cp input/l23_equi.rst7 output/
 
 cd output
 
-bash ~/cdk_scripts/run_md_gbsa_ptraj_gbsa.sh -c l23 -p 2 --test
+bash ~/cdk_scripts/run/run_md_ptraj_gbsa.sh -c l23 -p 2 --test
 
 cd ..
