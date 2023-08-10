3caf28c7927efb3efc9ed31cde0e362dbae74419
bnfalcone4 <bnfalcone@yahoo.es>
2023-08-10 17:05:44 +0100

    Changed prints

--- tests/test_run_md_ptraj_gbsa_part_n.sh
+++ tests/test_run_md_ptraj_gbsa_part_n.sh
@@ -6,6 +6,6 @@ cp input/l23_heat2.rst7 output/l23_3_equi.rst7
 
 cd input/
 
-python ../../run/run_md_ptraj_gbsa_part_n.py -c l23 -p 2 3 4 -r _3 --test YES
+python ../../run/run_md_ptraj_gbsa_part_n.py -c l23 -p 2 3 4 5 6 7 8 9 10 -r _3 --test YES
 
 cd ../
