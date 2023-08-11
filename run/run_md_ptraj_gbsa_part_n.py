d28c7e72739698cc335d4cdee88a1e02041e19ea
Bruno Falcone <bnfalcone@yahoo.es>
2023-08-11 12:30:49 +0100

    Modified arguments for ../run/run_md_ptraj_gbsa_part_n.py and tests

--- run/run_md_ptraj_gbsa_part_n.py
+++ run/run_md_ptraj_gbsa_part_n.py
@@ -2,6 +2,11 @@
 
 import argparse
 import os
+import sys
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(f'{file_dir}/../')
+from run import run
 
 parser = argparse.ArgumentParser(description='Run a part of an MD (i.e. sequential MDs)')
 
@@ -9,32 +14,39 @@ parser = argparse.ArgumentParser(description='Run a part of an MD (i.e. sequenti
 parser.add_argument('-c','--complex', help='Name of complexes to run', required=True)
 
 # Optional arguments
-parser.add_argument('-p', '--part_list', type=int, nargs='+', help='List of parts to run MD, etc, e.g. 2, 3', required=False)
+parser.add_argument('-fp', '--first_part', type=int, help='First part to run MD, etc, e.g. 2, 3', required=False)
+parser.add_argument('-lp', '--last_part', type=int, help='Last part to run MD, etc, e.g. 2, 3', required=False)
 parser.add_argument('-r','--repeat', default='NO', help='Repeat pattern, e.g. _2, _3', required=False)
 parser.add_argument('--test', default='NO', help='Do test run, i.e. very short MD', required=False)
 
 args = parser.parse_args()
 
-first_part = args.part_list[0]
+first_part = args.first_part
 
 cmd = f'sbatch --parsable ~/scripts/1gpu.sh ~/cdk_scripts/standard_md_part.py -c {args.complex} -p {first_part} -r {args.repeat} --test {args.test}'
 
-out = os.popen(cmd).read()
+print(cmd)
+
+out = run(cmd)
 
 jobid = out.splitlines()[0]
 
 print(f'Submitted batch job {jobid}')
 
-os.system(f'sbatch --dependency=afterok:{jobid} ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh -c {args.complex} -p {first_part} -r {args.repeat}')
+run(f'sbatch --dependency=afterok:{jobid} ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh -c {args.complex} -p {first_part} -r {args.repeat}')
 
-if len(args.part_list) > 1:
+if args.last_part is not None:
 
-    for part in args.part_list[1:]:
+    for part in range(args.first_part + 1, args.last_part + 1):
 
-        out = os.popen(f'sbatch --dependency=afterok:{jobid} --parsable ~/scripts/1gpu.sh ~/cdk_scripts/standard_md_part.py -c {args.complex} -p {part} -r {args.repeat} --test {args.test}').read()
+        cmd = f'sbatch --dependency=afterok:{jobid} --parsable ~/scripts/1gpu.sh ~/cdk_scripts/standard_md_part.py -c {args.complex} -p {part} -r {args.repeat} --test {args.test}'
+
+        print(cmd)
+
+        out = run(cmd)
 
         jobid = out.splitlines()[0]
 
         print(f'Submitted batch job {jobid}')
 
-        os.system(f'sbatch --dependency=afterok:{jobid} ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh -c {args.complex} -p {part} -r {args.repeat}')        
+        run(f'sbatch --dependency=afterok:{jobid} ~/scripts/1cpu.sh ~/cdk_scripts/run/run_ptraj_gbsa.sh -c {args.complex} -p {part} -r {args.repeat}')        
--- tests/test_run_md_ptraj_gbsa_part_n.sh
+++ tests/test_run_md_ptraj_gbsa_part_n.sh
@@ -1,11 +1,22 @@
 set -e
 
+# TEST 1
+
+first_part=2
+last_part=10
+
 rm -rf output/*
 cp input/l23.parm7 output/
 cp input/l23_heat2.rst7 output/l23_3_equi.rst7
 
-cd input/
+cd output/
 
-python ../../run/run_md_ptraj_gbsa_part_n.py -c l23 -p 2 3 4 -r _3 --test YES
+python ../../run/run_md_ptraj_gbsa_part_n.py -c l23 -fp $first_part -lp $last_part -r _3 --test YES
+
+for i in $(seq $first_part $last_part); do
+ls l23_3_equi$i.out l23_3_equi$i.rst7 l23_3_equi${i}_cent_strip.nc gbsa${i}_3/l23_gbsa${i}_3.dat
+done
 
 cd ../
+
+echo "TEST 1 PASSED"
