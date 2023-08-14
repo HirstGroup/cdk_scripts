b07db93cfee4a99632ad9c60b52f93741cd5d636
Bruno Falcone <bnfalcone@yahoo.es>
2023-08-14 16:13:11 +0100

    cluster_bs and tests

---
+++ ptraj_scripts/cluster_bs.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+import os
+import sys
+import textwrap
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(f'{file_dir}/../')
+from util.delete_md import count_frames
+from run import run
+
+
+def get_amber_range(resid_list, range_file):
+    """
+    Convert resid_list to amber format
+    """
+
+    range_tuple = list(get_range_tuple(resid_list))
+
+    print(range_tuple)
+
+    amber_range = ''
+
+    with open(range_file, 'w') as f:
+        for (x,y) in range_tuple:
+            if x!=y:
+                amber_range += f'{x}-{y},'
+                f.write(f'{x} to {y} ')
+            else:
+                amber_range += f'{x},'
+                f.write(f'{x} ')
+        f.write('\n')
+
+    return amber_range
+
+
+def get_range_tuple(resid_list):
+    """
+    Get list of tuples describing range
+    where tuple (a, b) means range goes from a to b inclusive
+    """
+    q = sorted(list(resid_list))
+    i = 0
+    for j in range(1,len(q)):
+        if q[j] > 1+q[j-1]:
+            yield q[i], q[j-1]
+            i = j
+    yield q[i], q[-1]
+
+
+def get_bs_mask(frames, LIG, parm, traj, interval=1):
+    """
+    Get mask of residues in binding site
+    """
+
+    if not os.path.exists('mask'):
+        os.makedirs('mask')
+
+    os.chdir('mask')
+
+    string = textwrap.dedent(f'''\
+    trajin ../{traj} 1 last {interval}
+    mask :{LIG}<:4.0 maskpdb resid_4a.pdb
+    ''')
+
+    with open('mask.ptraj', 'w') as f:
+        f.write(string)
+
+    run(f'cpptraj ../{parm} mask.ptraj', print_cmd=True)
+
+    resid_set = set()
+
+    for frame in range(1,frames+1):
+        df = np.genfromtxt(f'resid_4a.pdb.{frames}', names=None, skip_header=1, usecols=4, skip_footer=1, dtype=int)
+        resid_set.update(set(df))
+    
+    amber_range = get_amber_range(resid_set, 'resid_vmd.txt')
+
+    os.chdir('../')
+
+    return amber_range
+
+
+def cluster_bs(complex, epsilon=1, interval=1, repeat='', time='equi'):
+    """
+    Cluster trajectory by distance to ligand in binding site
+    """
+
+    parm = f'../{complex}_strip.parm7'
+    traj = f'../{complex}{repeat}_{time}_cent_strip.nc'
+
+    LIG = complex.upper()
+
+    folder = f'cluster{repeat}_{time}_{interval}'
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    os.chdir(folder)
+
+    frames = count_frames(parm, traj, verbose=True)
+
+    mask = get_bs_mask(frames, LIG, parm, traj, interval=interval)
+
+    string = textwrap.dedent(f'''\
+    trajin {traj} 1 last {interval}
+    cluster {complex} \
+    dbscan minpoints 2 epsilon {epsilon} sievetoframe \
+    rms :{LIG} nofit \
+    sieve 1 random \
+    pairdist pairdist.dat \
+    out cnumvtime.dat \
+    sil Sil \
+    summary summary.dat \
+    info info.dat \
+    cpopvtime cpopvtime.agr normframe \
+    repout {complex}_{time}_{epsilon} repfmt pdb       
+    ''')
+
+    with open('cluster.ptraj', 'w') as f:
+        f.write(string)
+
+    run(f'cpptraj {parm} cluster.ptraj')
+
+    os.chdir('../')
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Cluster a trajectory by distance to ligand in binding site')
+
+    # Required arguments
+    parser.add_argument('-c','--complex', help='Name of complexes to run', required=True)
+
+    # Optional arguments
+    parser.add_argument('-i', '--interval', type=int, help='Interval to analyse trajectories', required=False)
+
+    parser.add_argument('-r','--repeat', default='NO', help='Repeat pattern, e.g. _2, _3', required=False)
+    parser.add_argument('-t', '--time', default='equi', help='Time pattern to run second MD, etc, e.g. equi, equi2', required=False)
+
+    args = parser.parse_args()
+
+    cluster_bs()
+
---
+++ tests/test_cluster_bs.py
@@ -0,0 +1,59 @@
+import filecmp
+import sys
+
+sys.path.append('../')
+from ptraj_scripts.cluster_bs import *
+
+
+def test_get_range_tuple():
+
+	resid_list = [1,2,3,6,7,10]
+
+	range_tuple = list(get_range_tuple(resid_list))
+
+	assert range_tuple == [(1, 3), (6, 7), (10, 10)]	
+
+
+def test_get_amber_ranges():
+
+	resid_list = [1,2,3,6,7,10]
+
+	amber_range = get_amber_range(resid_list, 'output/range_file.txt')
+
+	print(amber_range)
+
+	assert amber_range == '1-3,6-7,10,'
+
+	assert filecmp.cmp('input/range_file.txt', 'output/range_file.txt') is True
+
+
+def test_get_bs_mask():
+
+	os.system('rm -rf output/*')
+	os.system('cp input/l23_strip.parm7 output/')
+	os.system('cp input/l23_equi_cent_strip.nc output/')
+
+	os.chdir('output/')
+
+	mask = get_bs_mask(2, 'L23', 'l23_strip.parm7', 'l23_equi_cent_strip.nc', interval=1)
+
+	print(mask)
+
+	os.chdir('../')
+
+	assert mask == '57-58,61,65,76,79-80,83-84,93,96-97,100-101,104,120,124,151,'
+
+
+def test_cluster_bs():
+
+	os.system('rm -rf output/*')
+	os.system('cp input/l23_strip.parm7 output/')
+	os.system('cp input/l23_equi_cent_strip_10.nc output/l23_equi_cent_strip.nc')
+
+	os.chdir('output/')
+
+	cluster_bs('l23')
+
+	os.chdir('../')
+
+test_cluster_bs()
