8499550e8a5067186fcb40f8fc5e3ac5f28692f8
Bruno Falcone <bnfalcone@yahoo.es>
2023-09-18 17:44:47 +0100

    Added print

--- run/run_md_ptraj_gbsa.py
+++ run/run_md_ptraj_gbsa.py
@@ -15,4 +15,6 @@ args = parser.parse_args()
 
 jobid=os.popen(f'sbatch --parsable ~/scripts/1gpu.sh bash ~/cdk_scripts/standard_md{args.part}.sh -c {args.complex} -r {args.repeat} --test {args.test}').read().splitlines()[0]
 
+print(f'Submitted batch job {jobid}')
+
 os.system(f'sbatch --dependency=afterok:{jobid} ~/scripts/1cpu.sh bash ~/cdk_scripts/run/run_ptraj_gbsa.sh -c {args.complex} -p {args.part} -r {args.repeat}')
