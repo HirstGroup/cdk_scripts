7e2c6cc7c78396843620e53cdac4ca03588ff96f
Bruno Falcone <bnfalcone@yahoo.es>
2023-09-19 12:20:50 +0100

    Added progress to rsync

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,5 @@
 # to copy from cluster
-rsync -rv --size-only --include='*/' --include='*strip.nc' --exclude='_*' --exclude='*.nc' --exclude='reference.frc'
+rsync -rvP --size-only --include='*/' --include='*strip.nc' --exclude='_*' --exclude='*.nc' --exclude='reference.frc'
 
 # to copy to run MDs to cluster
-rsync -rv --size-only --include='*/' --include='*.parm7' --include='*.rst7' --exclude='*'
+rsync -rvP --size-only --include='*/' --include='*.parm7' --include='*.rst7' --exclude='*'
